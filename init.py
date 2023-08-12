2aa1e893fd800741094e51e99d0be08aab2a605a
DrowKid <135056994+drowkid-1@users.noreply.github.com>
2023-08-11 23:09:06 -0600

    Update init.py
    Signed-off-by: DrowKid <135056994+drowkid-1@users.noreply.github.com>

--- init.py
+++ init.py
@@ -12,7 +12,7 @@ import os, sys
 
 
 #======================[Asignación del alias ]=========================[✓]
-os.system('mv .bwelcome .banngc .bannw .bannergit .banner-kid .banner-init ~/../home/AliasTermux/codes/.banners')
+os.system('mv .bwelcome .banngc .bannw .bannergit .banner-kid .banner-init ~/../home/Alias-Termux/codes/.banners')
 os.system("clear")
 p = "python3 "
 b = "cd codes/.banners;"
@@ -44,7 +44,7 @@ try:
 import os, sys, time, requests
 from time import sleep
 
-os.system('cd;cd KeysTermux/codes/.banners;python3 .banngc')
+os.system('cd;cd Alias-Termux/codes/.banners;python3 .banngc')
 sleep(1)
 
 gc = "git clone "
@@ -55,8 +55,8 @@ except OSError:
     print("")
 else:
     print("")
-os.system("mv .gc ~/../home/AliasTermux/codes")
-os.system('echo "cd;cd AliasTermux/codes;python3 .gc">>gc;chmod 775 gc;mv gc ~/../usr/bin')
+os.system("mv .gc ~/../home/Alias-Termux/codes")
+os.system('echo "cd;cd Alias-Termux/codes;python3 .gc">>gc;chmod 775 gc;mv gc ~/../usr/bin')
 #=======================================[WGET]=========================================[✓]
 
 try:
@@ -66,7 +66,7 @@ import os, sys, time, requests
 
 from time import sleep
 
-os.system('cd;cd AliasTermux/codes/.banners;python3 .bannw')
+os.system('cd;cd Alias-Termux/codes/.banners;python3 .bannw')
 sleep(1)
 
 w = "wget "
@@ -77,8 +77,8 @@ except OSError:
     print("")
 else:
     print("")
-os.system("mv .w ~/../home/AliasTermux/codes")
-os.system('echo "cd;cd AliasTermux/codes;python3 .w">>w;chmod 775 w;mv w ~/../usr/bin')
+os.system("mv .w ~/../home/Alias-Termux/codes")
+os.system('echo "cd;cd Alias-Termux/codes;python3 .w">>w;chmod 775 w;mv w ~/../usr/bin')
 #=======================================[extras]=========================================[✓]
-os.system('echo "cd;cd AliasTermux/codes;python3 alias.py">>alias.py;chmod 775 alias.py;mv alias.py ~/../usr/bin')
-os.system('echo "cd;cd AliasTermux/codes;python3 init.py">>keys-t;chmod 775 alias-t;mv alias-t ~/../usr/bin')
+os.system('echo "cd;cd Alias-Termux/codes;python3 alias.py">>alias-t;chmod 775 alias-t;mv alias-t ~/../usr/bin')
+os.system('echo "cd;cd Alias-Termux/codes;python3 init.py">>alias-termux;chmod 775 alias-termux;mv alias-termux ~/../usr/bin')
